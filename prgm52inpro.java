3968078e16b20c1badd63774f71043273daa8181
kamalee29 <kamalee.selvii24@gmail.com>
2016-08-29 03:13:26 -0700

    Add files via upload

---
+++ prgm52inpro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kamaleeswari
+ */import java.util.*;
+public class prgm52inpro {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[8];
+        System.out.println("enter 4 points as SQUARE coordinates");
+        for(int i=0;i<8;i++)
+            a[i]=sc.nextInt();
+        if(a[0]==a[2]&&a[4]==a[6]&&a[1]==a[7]&&a[3]==a[5])
+                    System.out.println("this  can form square");
+        else
+System.out.println("not an square making points");
+    }
+   
+}
